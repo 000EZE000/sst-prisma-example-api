35a42006963b4aff3c56fe0fec8f4139be43567e
000EZE000 <ezequiel.ignacio.sosa@gmail.com>
2024-05-14 17:11:51 -0300

    fix: descoment params input

--- sst.config.ts
+++ sst.config.ts
@@ -1,10 +1,11 @@
 import { SSTConfig } from "sst";
 import { ApiStack } from "./src/common/config_api";
 export default {
-  config(_input) {
+  config(input) {
     return {
       name: "tasks-api-prisma",
       region: "us-east-1",
+      stage: input?.stage === "prod" ? "prod" : "dev",
     };
   },
   stacks(app) {
